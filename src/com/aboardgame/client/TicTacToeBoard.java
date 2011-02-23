a33442bb9ef243fe83eda41c43f15a623bbee012
Mattias Fredsberg <mattias@fredsberg.se>
2011-02-23 23:44:43 +0100

    3x3 board

--- src/com/aboardgame/client/TicTacToeBoard.java
+++ src/com/aboardgame/client/TicTacToeBoard.java
@@ -3,27 +3,34 @@ package com.aboardgame.client;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class TicTacToeBoard extends Composite {
-    
-    private final Panel panel;
-    private final Button button;
-    
+
+    private final Button[][] buttons = new Button[3][3];
+
     public TicTacToeBoard() {
-        panel = new FlowPanel();
-        initWidget(panel);
-        
-        button = new Button("Press me");
-        panel.add(button);
-    }
-    
-    public Button getButton() {
-        return button;
+        VerticalPanel vPanel = new VerticalPanel();
+        initWidget(vPanel);
+        for (int i = 0; i < buttons.length; i++) {
+            HorizontalPanel hPanel = new HorizontalPanel();
+            vPanel.add(hPanel);
+            for (int j = 0; j < buttons[i].length; j++) {
+                String label = "" + (i * buttons.length + j);
+                Button button = new Button(label);
+                buttons[i][j] = button;
+                button.setSize("50px", "50px");
+                hPanel.add(button);
+            }
+        }
     }
 
-    public void addClickHandler(ClickHandler clickHandler) {
-        button.addClickHandler(clickHandler);
+    public void addClickHandler(ClickHandler handler) {
+        for (int i = 0; i < buttons.length; i++) {
+            for (int j = 0; j < buttons[i].length; j++) {
+                buttons[i][j].addClickHandler(handler);
+            }
+        }
     }
 }
